c9b7c4dd579a1e7113f84438b84c380d59e2bf7a
Jonathan <jjkiernan@gmail.com>
2015-11-28 10:59:40 +0000

    first commit

--- MoviesDB/javascript/controller.js
+++ MoviesDB/javascript/controller.js
@@ -1,17 +1,65 @@
 angular.module('movieDBControllers',[])
-.controller('MovieListController',function($scope,MovieListService) {
-// set scope with first and last names
-   var url = "https://api.themoviedb.org/3/movie/popular?api_key=35e16679c616a21b9ddebb66272c5902";
-   $scope.movieList = [];
-   
-
-   MovieListService.getList(url).then(
+.controller('MovieListController',function($scope,MovieListService,myMovieConfig) {
+      // set scope with first and last names
+      var url = myMovieConfig.moviesEndpoint + '/popular?api_key=' +
+      myMovieConfig.apiKey;
+      $scope.movieList = [];
+      $scope.loading = true;
+      MovieListService.getList(url).then(
       function(result){
-          $scope.movieList = result.data.results;  
+      $scope.movieList = result.data.results;
+      $scope.loading = false;
+}
+).catch(
+      function(error) {
+      console.log('error', error)
+      });
+})
 
-      }
-      ).catch(
-        function(error) { 
-          console.log('error', error)
-        });
+.controller('MovieTopRatedController' ,function($scope,MovieListService,myMovieConfig) {
+// set scope with first and last names
+var url = myMovieConfig.moviesEndpoint + '/top_rated?api_key=' +
+myMovieConfig.apiKey;
+$scope.movieList = [];
+$scope.loading = true;
+MovieListService.getList(url).then(
+function(result){
+$scope.movieList = result.data.results;
+$scope.loading = false;
+}
+).catch(
+function(error) {
+console.log('error', error)
 });
+}).controller( 'MovieNowPlayingController' ,function($scope,MovieListService,myMovieConfig) {
+// set scope with first and last names
+var url = myMovieConfig. moviesEndpoint + '/now_playing?api_key=' +
+myMovieConfig.apiKey;
+$scope.movieList = [];
+$scope.loading = true;
+MovieListService.getList(url).then(
+function(result){
+$scope.movieList = result.data.results;
+$scope.loading = false;
+}
+).catch(
+function(error) {
+console.log('error', error)
+});
+})
+.controller( 'MovieUpcomingController' ,function($scope,MovieListService,myMovieConfig) {
+var url = myMovieConfig.moviesEndpoint + '/upcoming?api_key=' +
+myMovieConfig.apiKey;
+$scope.movieList = [];
+$scope.loading = true
+
+  MovieListService.getList(url).then(
+function(result){
+$scope.movieList = result.data.results;
+$scope.loading = false;
+}
+).catch(
+function(error) {
+console.log('error', error)
+});
+})
